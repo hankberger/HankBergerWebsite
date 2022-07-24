026a4100531a287dd06cd44be76c66f254875b0c
hankberger <hanksberger@gmail.com>
2022-07-23 22:47:55 -0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Hank Berger | Frontend Developer</title>
+        <meta name="description" content="Frontend developer and artist. Focused on delivering quality engineering alongside great design." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        
+      </main>
+
+      <footer className={styles.footer}>
+      
+      </footer>
+    </div>
+  )
+}
+
+export default Home
