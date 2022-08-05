4392c1dd106d35e31697cfd26a6b26cea2d447f4
hankberger <hanksberger@gmail.com>
2022-08-04 22:18:49 -0500

    media webp

--- pages/3d.tsx
+++ pages/3d.tsx
@@ -7,13 +7,12 @@ const ThreeD: NextPage = () => {
     return(
         <div className={styles.page}>
              <Head>
-                <title>Hank Berger | Front-end Developer</title>
-                <meta name="description" content="Frontend developer and artist. Focused on delivering quality engineering alongside great design." />
+                <title>Hank Berger | 3D Art</title>
+                <meta name="description" content="3D Art Created by Hank Berger." />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <div className={styles.container}>
-            
                 <div className={styles.title}>
                     <h1>
                         3D Art
@@ -21,24 +20,28 @@ const ThreeD: NextPage = () => {
                 </div>
                 <div className={styles.desc}>
                     <p>3D Animation using Cinema4D, Redshift, Adobe After Effects, and Adobe Premiere. Commissions currently not open.</p>
-                    
                     <Link href='/'>
                         <div className={styles.linkcontainer}><p>&#8592; Back to Home</p></div>
                     </Link>
                 </div>
-                
                 <div className={styles.gallery}>
                     <div className={styles.item}>
-                        <video src="3d/Treesies.mp4" autoPlay muted controls playsInline></video>
+                        <video src="3d/tree1.mp4" autoPlay muted controls loop playsInline></video>
                     </div>
                     <div className={styles.item}>
-                        <video src="3d/desert.mp4" autoPlay muted controls playsInline></video>
+                        <video src="3d/cloud.mp4" autoPlay muted controls loop playsInline></video>
                     </div>
                     <div className={styles.item}>
-                        <video src="3d/hairdance2.mp4" autoPlay muted controls playsInline></video>
+                        <video src="3d/reflection.mp4" autoPlay muted controls loop playsInline></video>
                     </div>
                     <div className={styles.item}>
-                        <video src="3d/differentmasks.mp4" autoPlay muted controls playsInline></video>
+                        <video src="3d/clothy.mp4" autoPlay muted controls loop playsInline></video>
+                    </div>
+                    <div className={styles.item}>
+                        <video src="3d/frac1.mp4" autoPlay muted controls loop playsInline></video>
+                    </div>
+                    <div className={styles.item}>
+                        <video src="3d/eclipse.mp4" autoPlay muted controls loop playsInline></video>
                     </div>
                 </div>
             </div>
